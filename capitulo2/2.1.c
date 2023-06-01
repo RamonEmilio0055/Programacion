c04b0a22d212a065f733789493eaf2734f96f0e2
RamonEmilio0055 <133830626+RamonEmilio0055@users.noreply.github.com>
2023-06-01 11:23:50 -0400

    Create 2.1.c

---
+++ capitulo2/2.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Incremento de precio.
+El programa, al recibir como dato el precio de un producto importado,
+incrementa 11% el mismo si éste es inferior a $1,500.
+PRE y NPR: variable de tipo real. */
+int main(void)
+{
+    float PRE, NPR;
+    printf("Ingrese el precio del producto: ");
+    scanf("%f", &PRE);
+
+    // Resto del código...
+
+    return 0;
+}
